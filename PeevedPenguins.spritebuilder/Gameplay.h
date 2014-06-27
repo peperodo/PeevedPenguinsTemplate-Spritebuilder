2e6c1d285fa3eaa6008166f4260fb2d3924267be
Rodolfo Gomez <Home@Macintosh.local>
2014-06-27 18:42:22 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,16 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Rodolfo Gomez on 6/27/14.
+//  Copyright 2014 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Gameplay : CCSprite {
+    
+}
+
+@end
