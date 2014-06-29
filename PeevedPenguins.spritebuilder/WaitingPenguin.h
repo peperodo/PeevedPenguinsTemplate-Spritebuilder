9454e3c2bbc3118d63a123ae8a601553bd856449
Rodolfo Gomez <Home@Macintosh.local>
2014-06-29 15:05:10 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/WaitingPenguin.h
@@ -0,0 +1,16 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Rodolfo Gomez on 6/29/14.
+//  Copyright 2014 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface WaitingPenguin : CCSprite {
+    
+}
+
+@end
